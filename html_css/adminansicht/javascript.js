9214458e50c4ea4ca9e88d6a9c1be3d85055db43
Niklas Kugler <niklas.kugler03@gmail.com>
2025-06-02 14:35:16 +0200

    different colors for status

--- html_css/adminansicht/javascript.js
+++ html_css/adminansicht/javascript.js
@@ -8,3 +8,24 @@ allButtons.forEach(button => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".status").forEach(function (el) {
+        const status = el.textContent.trim();
+
+        switch (status) {
+            case "EINGEGANGEN":
+                el.classList.add("status-gelb");
+                break;
+            case "ANGENOMMEN":
+                el.classList.add("status-gruen");
+                break;
+            case "ABGELEHNT":
+                el.classList.add("status-rot");
+                break;
+            case "NEUEINZUREICHEN":
+                el.classList.add("status-orange");
+                break;
+        }
+    });
+});
