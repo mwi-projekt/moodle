30a73d13db1f9ce878150f58ec6fe1005f2d8f78
Niklas Kugler <niklas.kugler03@gmail.com>
2025-07-15 11:35:41 +0200

    Version

--- zuweisungsmatrix/version.php
+++ zuweisungsmatrix/version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_zuweisungsmatrix';
-$plugin->version = 2025062300;
+$plugin->version = 2025071500;
 $plugin->requires = 2021051700; // Mindest-Moodle-Version (Moodle 3.11)
