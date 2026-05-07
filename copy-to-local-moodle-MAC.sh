1fd4772dd5418096d33d9870fb4de1bd48b45517
tarikkadric <tarikkadric9@gmail.com>
2026-05-07 12:29:01 +0200

    Skript zum Verteilen der Plugins (MAC)

---
+++ copy-to-local-moodle-MAC.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ue
+
+BASE_PATH="${1:-/Applications/MAMP/htdocs}"
+ROOT_DEST="$BASE_PATH/moodle405"
+echo "Using Moodle root: $ROOT_DEST"
+
+[[ -d "$ROOT_DEST" ]] || {
+  echo "Destination root '$ROOT_DEST' does not exist."
+  exit 1
+}
+
+copy_plugin() {
+  local src="$1"
+  local subdir="$2"
+  local name="${3:-$1}"
+
+  local dest="$ROOT_DEST/$subdir/$name"
+  echo "Processing '$src' -> '$dest'"
+
+  [[ -d "$src" ]] || {
+    echo "  Skipping: source folder '$src' does not exist."
+    return
+  }
+
+  rm -rf "$dest"
+  mkdir -p "$ROOT_DEST/$subdir"
+  cp -r "$src" "$dest"
+
+  echo "  Done."
+}
+
+copy_plugin "dataform_plugin" "mod" "dataform"
+copy_plugin "io_plugin" "mod" "dhbwio"
+copy_plugin "zuweisungsmatrix" "local"
